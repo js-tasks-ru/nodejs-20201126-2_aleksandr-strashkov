ec8e5b44a47c2e66648b43e0ced9f62a741ed63b
Aleksandr Strashkov <avstrashkov@avito.ru>
2020-12-10 01:05:28 +0300

    05-http-server-streams/02-file-server-post

--- 05-http-server-streams/02-file-server-post/server.js
+++ 05-http-server-streams/02-file-server-post/server.js
@@ -1,16 +1,57 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const {pipeline} = require('stream');
+const LimitSizeStream = require('./LimitSizeStream');
+const LimitExceededError = require('./LimitExceededError');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
 
+  if (pathname.includes('/')) {
+    res.statusCode = 400;
+    return res.end('Not supported');
+  }
+
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'POST':
+      const writeStream = fs.createWriteStream(filepath, {
+        flags: 'wx',
+      });
+
+      writeStream.on('error', () => {
+        res.statusCode = 409;
+        return res.end();
+      });
+
+      writeStream.on('open', () => {
+        const limitSizeStream = new LimitSizeStream({limit: 1024 * 1024});
+
+        pipeline(
+            req,
+            limitSizeStream,
+            writeStream,
+            (err) => {
+              if (err) {
+                fs.unlinkSync(filepath);
+
+                if (err instanceof LimitExceededError) {
+                  res.statusCode = 413;
+                } else {
+                  res.statusCode = 500;
+                }
+              } else {
+                res.statusCode = 201;
+              }
+              res.end();
+            },
+        );
+      });
 
       break;
 
