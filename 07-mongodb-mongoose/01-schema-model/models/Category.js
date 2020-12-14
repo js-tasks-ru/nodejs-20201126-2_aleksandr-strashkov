cf65f53a0d7dbc5540204ae6fbf9c73e7641dfac
Aleksandr Strashkov <avstrashkov@avito.ru>
2020-12-14 22:57:54 +0300

    07-mongodb-mongoose/01-schema-model

--- 07-mongodb-mongoose/01-schema-model/models/Category.js
+++ 07-mongodb-mongoose/01-schema-model/models/Category.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
+const titleField = {
+  title: {
+    type: String,
+    required: true,
+  },
+};
+
 const subCategorySchema = new mongoose.Schema({
+  ...titleField,
 });
 
 const categorySchema = new mongoose.Schema({
+  ...titleField,
   subcategories: [subCategorySchema],
 });
 
