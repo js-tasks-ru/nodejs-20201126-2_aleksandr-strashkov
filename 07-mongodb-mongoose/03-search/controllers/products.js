f2f6760fe0f2724d66aba181f3917d9ebe21ab73
Aleksandr Strashkov <avstrashkov@avito.ru>
2020-12-18 00:01:29 +0300

    07-mongodb-mongoose/03-search

--- 07-mongodb-mongoose/03-search/controllers/products.js
+++ 07-mongodb-mongoose/03-search/controllers/products.js
@@ -1,3 +1,19 @@
-module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+const Product = require('../models/Product');
+
+module.exports.productsByQuery = async function productsByQuery(ctx) {
+  const {query} = ctx.request.query;
+
+  const products = await Product
+      .find(
+          {
+            $text: {$search: query},
+          },
+          {
+            score: {$meta: 'textScore'},
+          },
+      ).sort( {score: {$meta: 'textScore'}} );
+
+  ctx.body = {
+    products: products.map((product) => product.toJSON()),
+  };
 };
