ae309b8f514fd16a5eadb79a0bc1cf3db0ff33e1
Aleksandr Strashkov <avstrashkov@avito.ru>
2020-12-24 22:57:44 +0300

    09-email/01-registration

--- 09-email/01-registration/controllers/registration.js
+++ 09-email/01-registration/controllers/registration.js
@@ -1,11 +1,65 @@
-const { v4: uuid } = require('uuid');
+const {v4: uuid} = require('uuid');
 const User = require('../models/User');
 const sendMail = require('../libs/sendMail');
 
-module.exports.register = async (ctx, next) => {
+module.exports.register = async (ctx) => {
+  const token = uuid();
 
+  const {
+    email,
+    displayName,
+    password,
+  } = ctx.request.body;
+
+
+  try {
+    const user = new User({
+      email,
+      displayName,
+      verificationToken: token,
+    });
+
+    await user.setPassword(password);
+
+    await user.save();
+
+    await sendMail({
+      template: 'confirmation',
+      locals: {token},
+      to: email,
+      subject: 'Подтвердите почту',
+    });
+
+    ctx.body = {status: 'ok'};
+  } catch (err) {
+    ctx.status = 400;
+
+    const errors = Object.entries(err.errors)
+        .reduce((acc, [key, {message}]) => {
+          return {
+            ...acc,
+            [key]: message,
+          };
+        }, {});
+
+    ctx.body = {errors};
+  }
 };
 
-module.exports.confirm = async (ctx, next) => {
-  
+module.exports.confirm = async (ctx) => {
+  const {verificationToken} = ctx.request.body;
+
+  try {
+    const user = await User.findOne({verificationToken});
+    user.verificationToken = undefined;
+    await user.save();
+
+    ctx.body = {
+      token: verificationToken,
+    };
+  } catch (err) {
+    ctx.body = {
+      error: 'Ссылка подтверждения недействительна или устарела',
+    };
+  }
 };
